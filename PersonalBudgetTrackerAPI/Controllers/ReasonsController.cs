5faeccdacd707983675fc40ce7bc4445257e8092
swidan <eng.abdelrahman.swidan@gmail.com>
2026-03-04 17:47:51 +0200

    create reason CRUD actions

---
+++ PersonalBudgetTrackerAPI/Controllers/ReasonsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PersonalBudgetTrackerAPI.DatabaseContext;
+using PersonalBudgetTrackerAPI.Models;
+
+namespace PersonalBudgetTrackerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReasonsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReasonsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Reasons
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Reason>>> GetReason()
+        {
+            return await _context.Reason.ToListAsync();
+        }
+
+        // GET: api/Reasons/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Reason>> GetReason(Guid id)
+        {
+            var reason = await _context.Reason.FindAsync(id);
+
+            if (reason == null)
+            {
+                return NotFound();
+            }
+
+            return reason;
+        }
+
+        // PUT: api/Reasons/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReason(Guid id, Reason reason)
+        {
+            if (id != reason.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(reason).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReasonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Reasons
+        [HttpPost]
+        public async Task<ActionResult<Reason>> PostReason(Reason reason)
+        {
+            _context.Reason.Add(reason);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetReason", new { id = reason.Id }, reason);
+        }
+
+        // DELETE: api/Reasons/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReason(Guid id)
+        {
+            var reason = await _context.Reason.FindAsync(id);
+            if (reason == null)
+            {
+                return NotFound();
+            }
+
+            _context.Reason.Remove(reason);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ReasonExists(Guid id)
+        {
+            return _context.Reason.Any(e => e.Id == id);
+        }
+    }
+}
--- PersonalBudgetTrackerAPI/obj/Debug/net10.0/PersonalBudgetTrackerAPI.AssemblyInfo.cs
+++ PersonalBudgetTrackerAPI/obj/Debug/net10.0/PersonalBudgetTrackerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalBudgetTrackerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc3234976653217d93160d3f7255a3a0d0112aab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03b35ba3ea3f13845efdeaa11f9a91511fd919df")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalBudgetTrackerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalBudgetTrackerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
