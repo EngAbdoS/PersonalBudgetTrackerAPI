3fb4b42dd5e7458494d0d583abd0c4accfd06306
swidan <eng.abdelrahman.swidan@gmail.com>
2026-04-24 07:10:08 +0300

    handle auth response model

--- PersonalBudgetTrackerAPI/Models/Auth/AuthResponse.cs
+++ PersonalBudgetTrackerAPI/Models/Auth/AuthResponse.cs
@@ -2,7 +2,12 @@
 {
     public class AuthResponse
     {
-        public string Token { get; set; } = null!;
+        public string FullName { get; set; } = string.Empty;
+
+        public string Token { get; set; } = string.Empty;
+
+        public DateTime Expiration { get; set; }
+
         public string Message { get; set; } = string.Empty;
     }
 
