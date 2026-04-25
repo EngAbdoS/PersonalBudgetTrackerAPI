931c72244b10e73598aa1eb9128be51179856c06
swidan <eng.abdelrahman.swidan@gmail.com>
2026-04-25 04:00:58 +0300

    token model validation

--- PersonalBudgetTrackerAPI/DTOs/Auth/TokenModelDto.cs
+++ PersonalBudgetTrackerAPI/DTOs/Auth/TokenModelDto.cs
@@ -1,8 +1,24 @@
-﻿namespace PersonalBudgetTrackerAPI.DTOs.Auth
+﻿using FluentValidation;
+
+namespace PersonalBudgetTrackerAPI.DTOs.Auth
 {
     public class TokenModelDto
     {
         public string? Token { get; set; } = string.Empty;
         public string? RefreshToken { get; set; } = string.Empty;
     }
+
+    public class  TokenModelDtoValidator : FluentValidation.AbstractValidator<TokenModelDto>
+    {
+
+        public TokenModelDtoValidator()
+        {
+            RuleFor(x => x.Token)
+                .NotEmpty();
+            RuleFor(x => x.RefreshToken)
+                .NotEmpty();
+        }
+
+    }
+
 }
