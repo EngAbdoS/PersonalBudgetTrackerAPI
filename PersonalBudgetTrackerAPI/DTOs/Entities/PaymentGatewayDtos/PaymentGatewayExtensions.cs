31b9feeac60f972d8a8232f56907dd005aafc539
swidan <eng.abdelrahman.swidan@gmail.com>
2026-04-29 06:29:52 +0300

    PaymentGatewayExtension

---
+++ PersonalBudgetTrackerAPI/DTOs/Entities/PaymentGatewayDtos/PaymentGatewayExtensions.cs
@@ -0,0 +1,19 @@
+﻿using PersonalBudgetTrackerAPI.Models.Entities;
+
+namespace PersonalBudgetTrackerAPI.DTOs.Entities.PaymentGatewayDtos
+{
+    public static class PaymentGatewayExtensions
+    {
+        public static PaymentGatewayDto ToDto(this PaymentGateway pg)
+        {
+            return new PaymentGatewayDto
+            {
+                Id = pg.Id,
+                Title = pg.Title,
+                BankName = pg.BankName,
+                PaymentGatewayType = pg.PaymentGatewayType
+            };
+        }
+    }
+}   
+
