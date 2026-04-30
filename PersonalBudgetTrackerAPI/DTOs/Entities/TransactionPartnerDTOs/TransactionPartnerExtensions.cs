b1f88255b253a76d8b205258bcbf0b3eca1856eb
swidan <eng.abdelrahman.swidan@gmail.com>
2026-04-30 07:17:24 +0300

    TransactionPartnerExtensions

---
+++ PersonalBudgetTrackerAPI/DTOs/Entities/TransactionPartnerDTOs/TransactionPartnerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using PersonalBudgetTrackerAPI.Models.Entities;
+
+namespace PersonalBudgetTrackerAPI.DTOs.Entities.TransactionPartnerDTOs
+{
+    public static class TransactionPartnerExtensions
+    {
+        public static TransactionPartnerDto ToDto(this TransactionPartner entity)
+        {
+            return new TransactionPartnerDto
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Location = entity.Location,
+                Info = entity.Info,
+                Contact = entity.Contact
+            };
+        }
+    }
+}
