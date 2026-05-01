ded59b6ec3e4e1c3f55c6277c8f8c46f0c4beb32
swidan <eng.abdelrahman.swidan@gmail.com>
2026-05-01 07:19:14 +0300

    create TransactionFilterDto

---
+++ PersonalBudgetTrackerAPI/DTOs/Entities/TransactionDTOs/TransactionFilterDto.cs
@@ -0,0 +1,26 @@
+﻿using PersonalBudgetTrackerAPI.Common;
+using PersonalBudgetTrackerAPI.Models.Entities;
+
+namespace PersonalBudgetTrackerAPI.DTOs.Entities.TransactionDTOs
+{
+    public class TransactionFilterDto : PaginationQuery
+    {
+        public string? Type { get; set; }
+        public string? Search { get; set; }
+
+        public decimal? MinAmount { get; set; }
+        public decimal? MaxAmount { get; set; }
+
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+
+        public PaymentType? PaymentType { get; set; }
+
+        public Guid? PaymentGatewayId { get; set; }
+        public Guid? TransactionPartnerId { get; set; }
+        public Guid? CategoryId { get; set; }
+        public Guid? ReasonId { get; set; }
+
+
+    }
+}
