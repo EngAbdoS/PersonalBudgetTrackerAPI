5ce7ea341542d46d6ef8f102396f2bdaa3361eba
swidan <eng.abdelrahman.swidan@gmail.com>
2026-05-09 14:32:32 +0300

    DailySnapshot dto

---
+++ PersonalBudgetTrackerAPI/DTOs/FinanialPrefrancesDTOs/DailySnapshot/DailySnapshot.cs
@@ -0,0 +1,24 @@
+﻿namespace PersonalBudgetTrackerAPI.DTOs.FinanialPrefrancesDTOs.DailySnapshot
+{
+    public class DailySnapshot
+    {
+        public decimal TotalIncome { get; set; }
+        public decimal TotalExpense { get; set; }
+
+        public Dictionary<string, GatewaySnapshot> PaymentGateways { get; set; } = [];
+
+        public Dictionary<string, decimal> SpendingCategories { get; set; } = [];
+
+        public Dictionary<string, decimal> SpendingPartners { get; set; } = [];
+
+        public Dictionary<string, decimal> IncomeFromPartners { get; set; } = [];
+    }
+
+    public class GatewaySnapshot
+    {
+        public decimal TotalIncome { get; set; }
+        public decimal TotalExpense { get; set; }
+        public Dictionary<string, decimal> CategoriesSpendIn { get; set; } = [];
+        public Dictionary<string, decimal> PartnersSpendWith { get; set; } = [];
+    }
+}
